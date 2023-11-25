a82e5b68146ec3dee8b43338f5d9d210cf4258e3
xuborong <alexxu@wustl.edu>
2023-11-24 23:40:52 -0500

    theme first

--- Gemfile
+++ Gemfile
@@ -4,4 +4,15 @@ source "https://rubygems.org"
 
 # gem "rails"
 
-gem "jekyll", "~> 4.3"
+gem "jekyll", "~> 3.9"
+
+gem 'webrick', '>= 1.7'
+
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+
+group :jekyll_plugins do
+    gem 'minimal-mistakes-jekyll'
+    gem 'jekyll-data'
+    gem 'jekyll-include-cache'
+    gem 'github-pages'
+end
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Jill",
+        "excerpt":"Jill is an avid fruit grower based in the south of France.  ","categories": [],
+        "tags": [],
+        "url": "/authors/jill.html",
+        "teaser": null
+      },{
+        "title": "Ted",
+        "excerpt":"Ted has been eating fruit since he was baby.  ","categories": [],
+        "tags": [],
+        "url": "/authors/ted.html",
+        "teaser": null
+      },{
+        "title": "Bananas",
+        "excerpt":"A banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called “plantains”, distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but...","categories": [],
+        "tags": [],
+        "url": "/2018/08/20/bananas.html",
+        "teaser": null
+      },{
+        "title": "Apples",
+        "excerpt":"An apple is a sweet, edible fruit produced by an apple tree. Apple trees are cultivated worldwide, and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands...","categories": [],
+        "tags": [],
+        "url": "/2018/08/21/apples.html",
+        "teaser": null
+      },{
+        "title": "Kiwifruit",
+        "excerpt":"Kiwifruit (often abbreviated as kiwi), or Chinese gooseberry is the edible berry of several species of woody vines in the genus Actinidia. The most common cultivar group of kiwifruit is oval, about the size of a large hen’s egg (5–8 cm (2.0–3.1 in) in length and 4.5–5.5 cm (1.8–2.2 in)...","categories": [],
+        "tags": [],
+        "url": "/2018/08/22/kiwifruit.html",
+        "teaser": null
+      }]
